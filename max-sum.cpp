8dfa1e07d56ef13c7687736b9ce40af8753c4a44
Abhishek Ranjan <73399221+AbhihsekRanjan0408@users.noreply.github.com>
2020-10-24 23:07:57 +0530

    Update max-sum.cpp
    Minimum Sum Solution

--- max-sum.cpp
+++ max-sum.cpp
@@ -3,16 +3,16 @@ using namespace std;
 #define rec(a,i,n) for(int i=a;i<n;i++)
 main()
 {
-int arr[]={-1,2,4,-3,5,2,-5,2};
+int arr[]={-2,-3,-4,0,1,2,3,4};
 int n=*(&arr+1)-arr;
 int best=0;
 for (int a = 0; a < n; a++) {
 int sum = 0;
 for (int b = a; b < n; b++) {
 sum += arr[b];
-best = max(best,sum);
+best = min(best,sum);
 }
 }
-cout<<"The maximum sum of the elements is "<<best;
+cout<<"The minimum sum of the elements is "<<best;
 return 0;
 }
